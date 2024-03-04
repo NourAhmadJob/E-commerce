ae7f40736b92a277cf287cda4156eaef98fcece5
Nour Ahmad <nourjob202@gmail.com>
2024-03-04 22:24:44 +0300

    Building baseCase for full project

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:e_commerce/app/featcures/auth/presentation/bloc/login_bloc.dart';
+import 'package:e_commerce/app/featcures/splash_page/presentation/pages/splash_page.dart';
+import 'package:e_commerce/core/services_locator/di.dart';
+import 'package:e_commerce/core/utils/routes/route_manager.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  ServicesLocator().init();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      onGenerateRoute: RouteGenerator.getRoute,
+      initialRoute: Routes.splashRoute,
+      // theme: getApplicationTheme(),
+      home:  SplashPage(),
+    );
+  }
+}
